cdb73533f191527d7dd70d31eef21cd4fd3f224a
96_Qin <Qinpeng96@users.noreply.github.com>
2019-07-24 10:57:20 +0800

    int2str2list2str2int

---
+++ 7.adv_num.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        a = str(x)
+        y = list(a)
+        y.reverse()
+        if y[-1] == "-":
+            y.remove('-')
+            y.insert(0, '-')
+        if y[0] == 0:
+            y.remove('0')
+        y2 = ''.join(y)
+        z = int(str(y2))
+        if z < (-2**31) or z > (2**31-1):
+            z = 0
+        return z
