082942ffb5959e2bfd036e56cf8e10d51e2fb412
96_Qin <Qinpeng96@users.noreply.github.com>
2019-10-10 15:11:49 +0800

    10.10
    changed name of py and updatad a train

--- 4.findmediaarr.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int])->float:
-        nums1 = nums1 + nums2
-        nums1.sort()
-        i = (len(nums1)-1) // 2
-        j = len(nums1) // 2 
-        value = (nums1[i] + nums1[j]) / 2
-        return value
