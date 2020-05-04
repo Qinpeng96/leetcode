66a6a2a7efab90c8dc80002e01b2d0f88d974e9e
96_Qin <Qinpeng96@users.noreply.github.com>
2020-05-04 10:59:07 +0800

    add a example

--- 7.数字翻转.py
+++ 7.数字翻转.py
@@ -25,3 +25,33 @@ class Solution:
         if z < (-2**31) or z > (2**31-1):
             z = 0
         return z
+
+
+class Solution:
+    def reverse(self, x):
+        #首先对输入进行预处理[-9,9]w
+        if -10 < x <10: return x
+        str_x = str(x)
+        if str_x[-1] == 0: str_x = str_x[:-1]
+
+        if str_x[0] == '-':
+            str_x = "-" + str_x[:0:-1]
+        else:
+            str_x = str_x[::-1]
+        str_x = int(str_x)
+        if  2**31 - 1 < str_x or str_x < -(2**31):
+            return 0 
+        else:
+            return str_x
+
+
+if __name__ == "__main__":
+    s = Solution()
+    out = s.reverse(-132654)
+    print(out)
+"""
+    s = "abcdefg"
+    print(s[2::-1])#逆序输出[0:3]
+    print(s[:3:-1])#逆序输出最后三个
+    print(s[-2:-4:-1])#逆序输出倒数第二个和第三个
+"""
