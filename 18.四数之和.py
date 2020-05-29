d2bf3ab395dff0f3ec2680c561120f0aa15c4f22
96_Qin <Qinpeng96@users.noreply.github.com>
2020-05-29 16:22:24 +0800

    add

--- 18.四数之和.py
+++ 18.四数之和.py
@@ -23,28 +23,25 @@
 from typing import List 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        n = len(nums)
-        #首先排除数组小于4的情况
-        if n < 4: return []
-        nums.sort() #对数组进行排序
-        res = temp = []
-
-        for i in range(n-2):
-            for j in range(i+1,n-1):  #j从i+1开始
-                left,right = j+1,n-1  #left从j+1开始
+        res = []
+        count = len(nums)
+        if count < 4: return []
+        nums = sorted(nums)
+        for i in range(count-3):
+            for j in range(i+1,count-2):
+                left, right = j+1, count-1
                 while left < right:
-                    if nums[left] + nums[right] > target - nums[i] -nums[j]:
-                        right -= 1    #四个数之和大于目标，则右指针左移，减小和值
-                    elif nums[left] + nums[right] < target - nums[i] -nums[j]:
-                          left += 1  #四个数之和小于目标，则左指针右移，增大和值
-                    else:            #找到了目标数组，添加去重操作
-                        temp = [nums[i],nums[j],nums[left],nums[right]]
-                        if temp not in res:
-                            res.append(temp)
+                    temp = nums[i]+nums[j]+nums[left]+nums[right]
+                    if temp < target:
+                        left += 1
+                    elif temp > target:
+                        right -= 1
+                    else:
+                        if [nums[i],nums[j],nums[left],nums[right]] not in res:
+                            res.append([nums[i],nums[j],nums[left],nums[right]])
                         left += 1
                         right -= 1
-        return res
-        
+     
 if __name__ == "__main__":
     #初始化链表与数据
     data = [1, 0, -1, 0, -2, 2]
--- 20.有效括号.py
+++ 20.有效括号.py
@@ -12,4 +12,26 @@ class Solution:
             if c in dic: stack.append(c)                #入栈操作,加入 { [ ( ?
             elif dic[stack.pop()] != c: return False    
         return len(stack) == 1
-    
+"""
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dict = {'[':']','(':')','{':'}'}
+        num = len(s)
+        res = []
+        for i in range(num):
+            if len(res) == 0:
+                res.append(s[i])
+            elif res[-1] in dict and dict[res[-1]] == s[i]:
+                res.pop()
+            else:
+                res.append(s[i])
+        if len(res) == 0:
+            return True
+        else:
+            return False
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isValid("{([][]}()"))
+    # print(s.fourSum([0,0,0,0],0))
+"""
