05cd8f209a15bd070076d243ae192b877feeacd5
96_Qin <Qinpeng96@users.noreply.github.com>
2020-06-03 14:23:09 +0800

    add some solutions

--- 21.合成两个有序链表.py
+++ 21.合成两个有序链表.py
@@ -24,6 +24,26 @@ class Solution:
                 l1,l2 = l2,l1
             l1.next = self.mergeTwoLists(l1.next, l2)
         return l1 or l2
+###################################################
+#建立新的链表方法
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head1 = l1
+        head2 = l2
+        head = ListNode()
+        p = head
+        while head1.next and head2.next:
+            if head1.val < head2.val:
+                head = head1
+                head1 = head1.next
+            else:
+                head = head2
+                head2 = head2.next   
+        if head1 == None:
+            head.next = head2
+        else:
+            head.next = head1
+        return p.next
 
 
 
--- 22.括号生成.py
+++ 22.括号生成.py
@@ -29,6 +29,25 @@ class Solution:
                 double_tree(S + ')', left , right + 1)
         double_tree()
         return ans
+"""方法二 直接生成
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+
+        def generate(S:str, c, val:int,count:int):
+            S = S + c
+            if val < 0: return 0
+            if len(S) == 2*n and val == 0:
+                res.append(S)
+                return 0
+            if count > 2*n:
+                return 0
+            else:
+                generate(S ,'(',val+1,count+1)
+                generate(S , ')',val-1,count+1)
+        generate("",'(',1,1)
+        return res
+"""
 if __name__ == "__main__":
     a = Solution()
     print(a.generateParenthesis(3))
