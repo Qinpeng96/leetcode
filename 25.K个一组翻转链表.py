aa36801048e0486f8b0d4cfbbb4ae9f0a24c403e
96_Qin <Qinpeng96@users.noreply.github.com>
2020-06-11 22:32:41 +0800

    Update 25.K个一组翻转链表.py

--- 25.K个一组翻转链表.py
+++ 25.K个一组翻转链表.py
@@ -30,21 +30,29 @@ class ListNode:
 
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        p = head
+        piont = head
         index = 0
-        while index!=k and p:
-            p = p.next
-            index +=1
-        if index==k:
-            p = self.reverseKGroup(p,k)
-            while index>0:
-                temp = head.next
-                head.next = p 
-                p = head
-                head =temp
-                index -=1
-            head = p
-        return head
+        while (index != k and piont):#例如交换三个节点，运行之后 index=3，p.val= 4
+            piont = piont.next #p此时是作为尾节点存在，不会实质参与节点交换，只是起连接作用
+            index += 1
+        if index == k:#满足交换的个数条件
+            piont = self.reverseKGroup(piont, k)#递归，继续判断之后链表是否满足交换条件
+            """
+            例如：1->2->3->4 			 h=1, t=2, p=4
+			第一步：1->4； 2->3->4；		 h=2, t=3, p=1
+			第二步：2->1->4； 3->4；		 h=3, t=4, p=2
+			第三步：3->2->1->4；			 h=4, p=3 跳出循环后，将head=piont，head=3
+			"""
+            while (index != 0):#实际的交换过程
+                temp = head.next 
+                head.next = piont
+                piont = head
+                head = temp
+                index -= 1
+            head = piont
+
+        return head #返回自己的头节点作为P,因为是递归，所以是从后往前处理的，
+                    #返回的后一个链表的头节点作为本次运行的尾节点
 """
 递归分步说明：(假定链表1->2->3->4->5->6->7,k=3):
 第0轮：
