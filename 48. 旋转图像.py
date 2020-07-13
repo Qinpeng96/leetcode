9e7e0c15571b9b95890617a8213845332d01ce88
96_Qin <Qinpeng96@users.noreply.github.com>
2020-07-13 23:48:55 +0800

    Create 48. 旋转图像.py

---
+++ 48. 旋转图像.py
@@ -0,0 +1,82 @@
+"""
+48. 旋转图像
+给定一个 n × n 的二维矩阵表示一个图像。
+
+将图像顺时针旋转 90 度。
+
+说明：
+
+你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+
+示例 1:
+
+给定 matrix = 
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+原地旋转输入矩阵，使其变为:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+示例 2:
+
+给定 matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+], 
+
+原地旋转输入矩阵，使其变为:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+"""
+from typing import List
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        #对矩阵进行转置 T
+        for i in range(n):
+            for j in range(i,n):
+                matrix[i][j], matrix[j][i]= matrix[j][i], matrix[i][j]
+
+        #对转置后的矩阵对称变换一下
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+    
+if __name__ == "__main__":
+    s = Solution()
+    # nums = [
+    # [1,2,3],
+    # [4,5,6],
+    # [7,8,9]
+    # ]
+#     nums = [
+#   [ 1, 2, 3, 4],
+#   [ 5, 6, 7, 8],
+#   [ 9,10,11,12],
+#   [13,14,15,16]
+#     ]
+    nums = [
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+]
+    print(s.rotate(nums))
+
+
