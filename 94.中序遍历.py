3783ab9a5d1ffc2224c779d6e70f73a596caa86e
96_Qin <Qinpeng96@users.noreply.github.com>
2020-07-14 15:56:08 +0800

    update

--- 94.中序遍历.py
+++ 94.中序遍历.py
@@ -1,3 +1,19 @@
+"""
+94. 二叉树的中序遍历
+给定一个二叉树，返回它的中序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+"""
 # -*- coding: utf-8 -*-
 # Definition for a binary tree node.
 # class TreeNode:
@@ -5,6 +21,9 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+#前序遍历   中，左， 右
+#中序遍历   左，中， 右
+#后序遍历   左，右， 中
 
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
@@ -12,8 +31,8 @@ class Solution:
         def solve(root):
             if not root:
                 return 0
-            solve(root.left)
-            res.append(root.val)
-            solve(root.right)
+            solve(root.left)#       左
+            res.append(root.val)#   中
+            solve(root.right)#      右
         solve(root)
         return res
